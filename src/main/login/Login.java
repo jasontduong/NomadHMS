43d4c1f138ba4034d203fd406ead6414d2502b58
Jason Duong <jduong6@students.cs.ubc.ca>
2019-09-18 03:07:54 -0700

    added Login, Register, Quit options at initial start

--- src/main/login/Login.java
+++ src/main/login/Login.java
@@ -1,27 +1,49 @@
 package login;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.sound.sampled.Line;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Login {
-    private static String admin = "adminuser";
-    private static String adminpw = "admin$";
+
+    public static String admin = "adminuser";
+    public static String adminpw = "admin$";
 
     public static void main(String[] args) {
+
 
         Scanner reader = new Scanner(System.in);
 
 
         while (true) {
-            System.out.print("Type your username: ");
-            String id1 = reader.nextLine();
-            System.out.print("Type your password: ");
-            String pw1 = reader.nextLine();
+            System.out.print("What do you want to do? Login, Register, or Quit?: ");
+            String choice1 = reader.nextLine();
 
-            if (id1.equals(admin) && pw1.equals(adminpw)) {
-                System.out.println(login(id1));
+            if (choice1.equals("Quit")) {
+                System.out.println("Okay bye!");
                 break;
             }
-            invalid(id1, pw1);
+
+            if (choice1.equals("Login")) {
+
+                System.out.print("Type your username: ");
+                String id1 = reader.nextLine();
+                System.out.print("Type your password: ");
+                String pw1 = reader.nextLine();
+
+                if (id1.equals(admin) && pw1.equals(adminpw)) {
+                    System.out.println(login(id1));
+                }
+                if (id1.equals(id1.equals("Poop")) && pw1.equals("poop")) {
+                    System.out.println(login(id1));
+                    break;
+                }
+                invalid(id1, pw1);
+            }
         }
     }
 
@@ -38,5 +60,13 @@ public class Login {
             System.out.println("Your password is invalid!");
 
         }
+
     }
+
+    public void Register(String id1, String pw1) {
+        id1 = "Poop"; //needs to take user input
+        pw1 = "poop"; //needs to take user input
+        System.out.println("Registered successfully!");
+    }
+
 }
