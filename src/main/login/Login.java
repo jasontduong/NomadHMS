7773188d4f7a16ca7e1f509a5e781c76e3c63724
Jason Duong <jduong6@students.cs.ubc.ca>
2019-09-18 14:09:28 -0700

    added Login, Register, Quit options at initial start

--- src/main/login/Login.java
+++ src/main/login/Login.java
@@ -1,11 +1,5 @@
 package login;
 
-import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
-
-import javax.sound.sampled.Line;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Login {
