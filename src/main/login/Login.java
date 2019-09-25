07a9f72d8b536a10b5fccd9c02877272cb942e3c
jduong6 <oLimer12#18309154>
2019-09-25 05:27:55 -0700

    made logmein object, took out from main

--- src/main/login/Login.java
+++ src/main/login/Login.java
@@ -9,11 +9,11 @@ public class Login {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        Scanner reader1 = new Scanner(System.in);
 
         while (true) {
-            System.out.print("What do you want to do? Login, Register, or Quit?: ");
-            String choice1 = reader.nextLine();
+            System.out.print("What do you want to do? Login, or Quit?: ");
+            String choice1 = reader1.nextLine();
 
             if (choice1.equals("Quit")) {
                 System.out.println("Okay bye!");
@@ -21,28 +21,28 @@ public class Login {
             }
 
             if (choice1.equals("Login")) {
-
-                System.out.print("Type your username: ");
-                String id1 = reader.nextLine();
-                System.out.print("Type your password: ");
-                String pw1 = reader.nextLine();
-
-                if (id1.equals(admin) && pw1.equals(adminpw)) {
-                    System.out.println(login(id1));
-                    break;
-                }
-                if (id1.equals("Poop") && pw1.equals("poop")) {
-                    System.out.println(login(id1));
-                    break;
-                }
-                invalid(id1, pw1);
+                logmein();
+                break;
             }
         }
     }
 
-    // if login is successful
-    private static String login(String id1) {
-        return "Welcome to JasonHMS " + id1 + "!";
+    // logs user in
+    private static void logmein() {
+        Scanner reader2 = new Scanner(System.in);
+
+        System.out.print("Type your username: ");
+        String id1 = reader2.nextLine();
+        System.out.print("Type your password: ");
+        String pw1 = reader2.nextLine();
+
+        if (id1.equals(admin) && pw1.equals(adminpw)) {
+            System.out.println(login(id1));
+        }
+        if (id1.equals("Poop") && pw1.equals("poop")) {
+            System.out.println(login(id1));
+        }
+        invalid(id1, pw1);
     }
 
     // if username or password does not match
@@ -54,9 +54,9 @@ public class Login {
         }
     }
 
-    //  public void register() {
-    //  String id2 = "Poop"; //needs to take user input
-    //  String pw2 = "poop"; //needs to take user input
-    //  System.out.println("Registered successfully!");
-    //  }
+    // if login is successful
+    private static String login(String id1) {
+        return "Welcome to JasonHMS " + id1 + "!";
+    }
+
 }
