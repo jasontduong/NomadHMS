0e6af80c8edab7cb5ed3fd13ec397171a0000ff8
jduong6 <oLimer12#18309154>
2019-09-25 05:34:58 -0700

    -removed methods from main -previous methods from main made into objects

--- src/main/login/Login.java
+++ src/main/login/Login.java
@@ -8,7 +8,10 @@ public class Login {
     private static String adminpw = "admin$";
 
     public static void main(String[] args) {
+        choice();
+    }
 
+    private static void choice() {
         Scanner reader1 = new Scanner(System.in);
 
         while (true) {
@@ -21,36 +24,39 @@ public class Login {
             }
 
             if (choice1.equals("Login")) {
-                logmein();
+                attemptlogin();
                 break;
             }
         }
     }
 
     // logs user in
-    private static void logmein() {
+    private static void attemptlogin() {
         Scanner reader2 = new Scanner(System.in);
+        while (true) {
+            System.out.print("Type your username: ");
+            String id1 = reader2.nextLine();
+            System.out.print("Type your password: ");
+            String pw1 = reader2.nextLine();
 
-        System.out.print("Type your username: ");
-        String id1 = reader2.nextLine();
-        System.out.print("Type your password: ");
-        String pw1 = reader2.nextLine();
-
-        if (id1.equals(admin) && pw1.equals(adminpw)) {
-            System.out.println(login(id1));
+            if (id1.equals(admin) && pw1.equals(adminpw)) {
+                System.out.println(login(id1));
+                break;
+            }
+            if (id1.equals("Poop") && pw1.equals("poop")) {
+                System.out.println(login(id1));
+                break;
+            }
+            invalid(id1, pw1);
         }
-        if (id1.equals("Poop") && pw1.equals("poop")) {
-            System.out.println(login(id1));
-        }
-        invalid(id1, pw1);
     }
 
     // if username or password does not match
     private static void invalid(String id1, String pw1) {
         if (!id1.equals(admin)) {
-            System.out.println("Your username is invalid!");
+            System.out.println("Your username is invalid! Try again!");
         } else if (!pw1.equals(adminpw)) {
-            System.out.println("Your password is invalid!");
+            System.out.println("Your password is invalid! Try again!");
         }
     }
 
