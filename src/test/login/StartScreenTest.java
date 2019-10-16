fcc007bea1de27697152289cff2907cc93c78ca0
Jason Duong <jduong6@students.cs.ubc.ca>
2019-10-16 14:18:52 -0700

    -not compiling

--- src/test/login/StartScreenTest.java
+++ src/test/login/StartScreenTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static junit.framework.TestCase.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StartScreenTest {
